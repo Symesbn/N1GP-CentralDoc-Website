5e772a8a0bfda05601bafff1dce178d2c23601b6
Symesbn <43187354+Symesbn@users.noreply.github.com>
2022-06-14 11:20:29 -0400

    Created Tango Software Guide page. About 40% Finished.

---
+++ SGTango.php
@@ -0,0 +1,41 @@
+<?php
+   $title = "Tango";
+   $date = "2022-06-14";
+   $filename = "SGTango.php";
+   $banner = "N1GP Central Document";
+   $description = "Tango page of central doc";
+   $author = "Symes"
+?>
+
+<?php include 'CentralHeader.php';?>
+
+<p>This guide will show you how to play Battle Netowkr online. It covers the setup process for Tango, and shows how to connect to other players in Tango.</p>
+
+<br>
+
+<h2>Downloading and setting up Tango</h2>
+
+<p>Download Tango from the Website: <a href="https://tangobattle.com/">https://tangobattle.com/</a></p>
+
+<img src="Images/SG_Tango/TangoSite.png" width="1000" height="585">
+
+<p>Run the Tango installer and launch Tango when the installation is finished.</p>
+<p>It should look like this if installed correctly:</p>
+
+<img src="Images/SG_Tango/TangoWelcome.png" width="650" height="500">
+
+<p>You can click the Open Folder button to move your dumped froms to the &ltuser&gt\Documents\Tango\roms folder.<br>
+	Note: For details on dumping ROMs please refer to the <a href="http://localhost/Central%20Doc/FAQ.php#IntroQ8">FAQ Section</a>.</p>
+	
+<p>Your roms folder should look like this:</p>
+	
+<img src="Images/SG_Tango/TangoRoms.png">
+	
+<p>The roms contained in this folder should be "vanilla" games, i.e. no patches should be applied.</p>
+
+<p>Once this is finished click I'm done!</p>
+
+<img src="Images/SG_Tango/TangoSaves.png">
+
+
+<?php include 'Footer.php' ?>
